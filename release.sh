96a5373fe272d1e42c97d4affc9c9beefc328539
Aaron Schlesinger <aschlesinger@paypal.com>
2014-08-25 16:00:10 -0700

    adding a script for releasing

---
+++ release.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+RELEASE_BRANCH=$1
+echo "preparing to release $RELEASE_BRANCH"
+git branch -u origin/$RELEASE_BRANCH
+git pull
+git config branch.$RELEASE_BRANCH.remote origin
+git config branch.$RELEASE_BRANCH.merge refs/heads/$RELEASE_BRANCH
+sbt 'release-cross with-defaults'
